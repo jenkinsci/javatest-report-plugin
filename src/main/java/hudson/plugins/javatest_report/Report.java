cb77d0feb518acfa8bcd87d98163940507985f42
Carroll Chiou <cchiou@cloudbees.com>
2021-04-30 13:43:44 -0600

    upgrade to commons-digester3

--- src/main/java/hudson/plugins/javatest_report/Report.java
+++ src/main/java/hudson/plugins/javatest_report/Report.java
@@ -20,9 +20,10 @@
 package hudson.plugins.javatest_report;
 
 import hudson.model.AbstractBuild;
-import org.apache.commons.digester.Digester;
+import org.apache.commons.digester3.Digester;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 
@@ -64,6 +65,17 @@ public final class Report extends TestCollection<Report,Suite> {
      */
     public void add( File reportXml ) throws IOException, SAXException {
         Digester digester = new Digester();
+        if (!Boolean.getBoolean(this.getClass().getName() + ".UNSAFE")) {
+            digester.setXIncludeAware(false);
+            try {
+                digester.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+                digester.setFeature("http://xml.org/sax/features/external-general-entities", false);
+                digester.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+                digester.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            } catch (ParserConfigurationException ex) {
+                throw new SAXException("Failed to securely configure xml digester parser", ex);
+            }
+        }
         digester.setClassLoader(getClass().getClassLoader());
 
         digester.push(this);
